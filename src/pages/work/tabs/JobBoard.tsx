30dfd7e76c32cf9a488416e06282025957033d75
0xMayflower <0xMayflower@protonmail.com>
2021-05-17 00:17:04 +0900

    add faq to jobboard

--- src/pages/work/tabs/JobBoard.tsx
+++ src/pages/work/tabs/JobBoard.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Page from "../../../layouts/Page";
-import { Button, Col, Image, Nav, Row, Tab, Tabs } from "react-bootstrap";
+import { Button, Col, Nav, Row, Tab } from "react-bootstrap";
 import { useWorkhardContracts } from "../../../providers/WorkhardContractProvider";
 import { BigNumber } from "ethers";
 import { ProjectBox } from "../../../components/contracts/job-board/ProjectBox";
@@ -94,6 +93,12 @@ export const JobBoard: React.FC = () => {
         <Col sm={9}>
           <Tab.Content>
             <Tab.Pane eventKey="activeProjects">
+              {activeProjects.length === 0 && (
+                <p>
+                  No active project exists! Post a new one or approve the
+                  pending projects.
+                </p>
+              )}
               {activeProjects.map((id) => (
                 <div key={id.toString()}>
                   <ProjectBox projId={id} active={true} />
@@ -102,6 +107,7 @@ export const JobBoard: React.FC = () => {
               ))}
             </Tab.Pane>
             <Tab.Pane eventKey="inactiveProjects">
+              {inactiveProjects.length === 0 && <p>Empty!</p>}
               {inactiveProjects.map((id) => (
                 <div key={id.toString()}>
                   <ProjectBox projId={id} active={false} />
@@ -127,6 +133,44 @@ export const JobBoard: React.FC = () => {
               </Row>
               <br />
               <Button variant={"info"} children="Trade $COMMIT on Uniswap" />
+            </Tab.Pane>
+            <Tab.Pane eventKey="faq">
+              <h5>
+                <strong>How can I work?</strong>
+              </h5>
+              <p>
+                You can get in touch with the budget owner via the url on the
+                job post. Or, visit Workhard's official discord channel to get
+                more information.
+              </p>
+              <h5>
+                <strong>How can I post a job?</strong>
+              </h5>
+              <p>
+                An approved project's budget owner has an ability to mint
+                $COMMIT tokens by reserving stable coins. To manage the abusing
+                usage of the job board, governance(Workers Union) can approve or
+                disapprove project. Therefore, to get whitelisted, visit
+                Workhard discord channel and explain your plan to people first.
+              </p>
+              <h5>
+                <strong>How can my posted job get approved?</strong>
+              </h5>
+              <p>
+                For the first 4 weeks after the launch date, dev's multisig
+                wallet has the control for the approval and disapproval of a
+                post. After 4 weeks, Workers Union decides it via onchain
+                voting.
+              </p>
+              <h5>
+                <strong>How to use this job board?</strong>
+              </h5>
+              <p>
+                App shows "project admin tool" menu to the budget owner. In the
+                admin tool, you can compensate people with $COMMIT and the
+                contributors can use $COMMIT to redeem for $DAI or burn to get
+                $VISION.
+              </p>
             </Tab.Pane>
           </Tab.Content>
         </Col>
