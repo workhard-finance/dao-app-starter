f399458d6041edea22bf247177e30a80a2577e9b
0xMayflower <0xMayflower@protonmail.com>
2021-05-17 00:42:41 +0900

    add faq to stable reserve

--- src/pages/work/StableReserve.tsx
+++ src/pages/work/StableReserve.tsx
@@ -30,6 +30,42 @@ const StableReserve: React.FC = () => {
               <br />
               <Button variant={"info"} children="Trade $COMMIT on Uniswap" />
             </Tab.Pane>
+            <Tab.Pane eventKey="faq">
+              <h5>
+                <strong>What can I do with $COMMIT?</strong>
+              </h5>
+              <p>
+                Contributors get compensation by $COMMIT instead of $DAI, but
+                they still can redeem for $DAI as the stable reserve always
+                reserves enough $DAI for the redemption. Otherwise, you can
+                choose to burn to get $VISION emission. Around 40~50% of the
+                total VISION emission goes to whom contributed to the protocol.
+                As contributors burn their $COMMIT to get $VISION, the Stable
+                Reserve has extra $COMMIT that can be paid to other
+                contributors. Therefore, the mechanism between $VISION and
+                $COMMIT works like a liquid stock option.
+                <br />
+                <br />
+                In conclusion, if you need money right now redeem $COMMIT for
+                $DAI at the Stable Reserve. Or if you believe in the success of
+                this protocol, choose to burn your $COMMIT to get more $VISION.
+              </p>
+              <h5>
+                <strong>Why $COMMIT is pegged between $1 ~ $2?</strong>
+              </h5>
+              <p>
+                Although, you can earn $COMMIT by contributing to the protocol,
+                you can also buy them via secondary markets like Sushiswap. As a
+                big portion of $VISION emission goes to $COMMIT burners, $COMMIT
+                price can be overheated in the market. By the way, it is nice
+                for you that your $COMMIT deserves more than 1 $DAI. However,
+                Stable Reserve also offers an option to buy $COMMIT directly
+                through it with the price of $2 DAI per 1 COMMIT. So, if $COMMIT
+                gets too expensive, people can do arbitrage between the reserve
+                and the secondary markets and the Stable Reserve can mint more
+                $COMMITs for grants.
+              </p>
+            </Tab.Pane>
           </Tab.Content>
         </Col>
       </Row>
