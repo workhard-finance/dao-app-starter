659e5b7dbe0d51bd3353a074ab05b2bdf1c1d629
0xMayflower <0xMayflower@protonmail.com>
2021-05-17 18:28:27 +0900

    add faq to nfts menu again

--- src/pages/nfts/index.tsx
+++ src/pages/nfts/index.tsx
@@ -89,13 +89,13 @@ const Store: React.FC = () => {
           </Row>
         </Tab>
         <Tab eventKey="all" title="All" style={{ marginTop: "1rem" }}>
+          {allProducts.length === 0 && (
+            <p>Coming soon :) We won't let you wait too long.</p>
+          )}
           <Alert variant="warning">
             This is a permisionless marketplace. Please DYOR for each product
             registered here.
           </Alert>
-          {allProducts.length === 0 && (
-            <p>Coming soon :) We won't let you wait too long.</p>
-          )}
           <Row>
             {allProducts.map((tokenId) => (
               <Col key={`all-${tokenId}`} md={4}>
@@ -105,11 +105,54 @@ const Store: React.FC = () => {
             ))}
           </Row>
         </Tab>
+        <Tab eventKey="faq" title="FAQ" style={{ marginTop: "1rem" }}>
+          <h5>
+            <strong>How can I buy NFTs?</strong>
+          </h5>
+          <p>
+            You can buy NFT products here using $COMMIT. You can earn $COMMIT by
+            working or buy them via Sushiswap and the Stable Reserve.
+          </p>
+          <Button variant="info">Go to Sushiswap</Button>{" "}
+          <Button variant="warning">Go to the Stable Reserve</Button>
+          <br />
+          <br />
+          <h5>
+            <strong>Who are making these NFTs?</strong>
+          </h5>
+          <p>
+            Anyone who wants to contribute to the protocol can sell NFTs here in
+            the form of ERC1155 and share the revenue with RIGHT(veVISION)
+            holders. If you're a VISION holder, try to be a manufacturer and
+            make the intrinsic value to the protocol.
+          </p>
+          <Button as={Link} to={`/manufacturer/new`} variant="outline-primary">
+            Be a manufacturer
+          </Button>
+          <br />
+          <br />
+          <h5>
+            <strong>What are the supported token types?</strong>
+          </h5>
+          <p>
+            Currently, it mints NFTs on a single ERC1155 contract. In the
+            future, we'll support more various types of NFTs (obviously ERC721)
+            and sales types like auctions.
+            <br />
+            For your information, you can consider ERC1155 as a limited edition
+            of Nike Sneakers while ERC721 is a unique artwork.
+          </p>
+          <h5>
+            <strong>How does it determine the token ID?</strong>
+          </h5>
+          <p>
+            If you launch a product here, you will have a product metadata that
+            includes the information about the name, description, and image uri.
+            The token id is determined by the hash of the metadata uri and
+            manufacturer's address.
+          </p>
+        </Tab>
       </Tabs>
-      <hr />
-      <Button as={Link} to={`/manufacturer/new`} variant="outline-primary">
-        Be a manufacturer
-      </Button>
     </Page>
   );
 };
