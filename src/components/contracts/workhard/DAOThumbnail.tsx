2d43ea44505bcf32ea97236b5bc7aee19c085f44
0xMayflower <0xMayflower@protonmail.com>
2021-06-04 02:15:45 +0900

    beautify dao thumbnails

--- src/components/contracts/workhard/DAOThumbnail.tsx
+++ src/components/contracts/workhard/DAOThumbnail.tsx
@@ -9,7 +9,8 @@ import {
 } from "../../../utils/utils";
 import { useToasts } from "react-toast-notifications";
 import { useIPFS } from "../../../providers/IPFSProvider";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 export interface DAOThumbnailProps {
   daoId?: BigNumberish;
@@ -29,13 +30,13 @@ export const DAOThumbnail: React.FC<DAOThumbnailProps> = ({
   onClick,
 }) => {
   const workhardCtx = useWorkhard();
-  const history = useHistory();
   const { addToast } = useToasts();
   const { ipfs } = useIPFS();
   const [daoMetadata, setDAOMetadata] = useState<DAOMetadata | undefined>(
     metadata
   );
   const [imageURI, setImageURI] = useState<string>();
+  const [description, setDescription] = useState<string>();
 
   useEffect(() => {
     if (workhardCtx && daoId) {
@@ -61,6 +62,7 @@ export const DAOThumbnail: React.FC<DAOThumbnailProps> = ({
       fetchProjectMetadataFromIPFS(ipfs, daoMetadata.uri)
         .then((projectMetadata) => {
           setImageURI(projectMetadata.image);
+          setDescription(projectMetadata.description);
         })
         .catch(errorHandler(addToast));
     }
@@ -68,12 +70,24 @@ export const DAOThumbnail: React.FC<DAOThumbnailProps> = ({
 
   return (
     <Card onClick={onClick} style={{ cursor: onClick ? "pointer" : undefined }}>
-      <Card.Img
-        style={{ borderRadius: 0 }}
-        src={uriToURL(
-          imageURI || "QmZ6WAhrUArQPQHQZFJBaQnHDcu5MhcrnfyfX4uwLHWMj1"
-        )}
-      />
+      <Card.Body>
+        <Row>
+          <Col xs={5}>
+            <Card>
+              <Card.Img
+                style={{ borderRadius: 0 }}
+                src={uriToURL(
+                  imageURI || "QmZ6WAhrUArQPQHQZFJBaQnHDcu5MhcrnfyfX4uwLHWMj1"
+                )}
+              />
+            </Card>
+          </Col>
+          <Col xs={7} style={{ height: "5rem" }}>
+            <p style={{ overflow: "auto", maxHeight: "100%" }}>{description}</p>
+          </Col>
+        </Row>
+      </Card.Body>
+
       <Card.Footer>
         {daoMetadata?.name}({daoMetadata?.symbol})
       </Card.Footer>
