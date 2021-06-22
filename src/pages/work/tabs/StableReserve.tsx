2ff113567d0cb1a978f0b877631ddd4dc27ee3fd
0xMayflower <0xMayflower@protonmail.com>
2021-06-22 21:39:30 +0900

    show commit market price

--- src/pages/work/tabs/StableReserve.tsx
+++ src/pages/work/tabs/StableReserve.tsx
@@ -1,26 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
 import { useToasts } from "react-toast-notifications";
+import { constants, Contract, ethers } from "ethers";
 import { BuyCommit } from "../../../components/contracts/stable-reserve/BuyCommit";
 import { RedeemCommit } from "../../../components/contracts/stable-reserve/RedeemCommit";
 import { useBlockNumber } from "../../../providers/BlockNumberProvider";
 import { useWorkhard } from "../../../providers/WorkhardProvider";
-import { getPriceFromCoingecko } from "../../../utils/coingecko";
 import { errorHandler } from "../../../utils/utils";
+import { getPool, getPoolAddress, getPoolContract } from "../../../utils/uniV3";
 
 const StableReserve: React.FC = () => {
   const workhardCtx = useWorkhard();
   const [commitPrice, setCommitPrice] = useState<number>();
   const { blockNumber } = useBlockNumber();
   const { addToast } = useToasts();
+  const [uniV3Pool, setUniV3Pool] = useState<Contract | undefined>();
 
   useEffect(() => {
-    if (!!workhardCtx) {
-      getPriceFromCoingecko(workhardCtx.dao.commit.address)
-        .then(setCommitPrice)
+    if (!!workhardCtx && workhardCtx.web3.chainId === 1) {
+      Promise.all(
+        [10000, 3000, 500].map((fee) =>
+          getPoolAddress(
+            workhardCtx.web3.library,
+            workhardCtx.dao.baseCurrency.address,
+            workhardCtx.dao.commit.address,
+            fee
+          )
+        )
+      ).then(async (pools) => {
+        const poolAddress = pools.find((p) => p !== constants.AddressZero);
+        if (poolAddress) {
+          const pool = await getPoolContract(
+            poolAddress,
+            workhardCtx.web3.library
+          );
+          setUniV3Pool(pool);
+        }
+      });
+    } else {
+      setUniV3Pool(undefined);
+    }
+  }, [workhardCtx, workhardCtx?.web3.chainId]);
+
+  useEffect(() => {
+    if (!!uniV3Pool) {
+      getPool(uniV3Pool)
+        .then(async (pool) => {
+          setCommitPrice(parseFloat(pool.token1Price.toFixed()));
+        })
         .catch(errorHandler(addToast));
     }
-  }, [workhardCtx, blockNumber]);
+  }, [uniV3Pool, blockNumber]);
+
   return (
     <Row>
       <Col md={5} style={{ marginTop: "1rem" }}>
@@ -32,13 +63,6 @@ const StableReserve: React.FC = () => {
             Market Price
           </Card.Header>
           <Card.Body>
-            {commitPrice && commitPrice !== NaN && (
-              <p>
-                {commitPrice}{" "}
-                {workhardCtx?.metadata.baseCurrencySymbol || "DAI"}/
-                {workhardCtx?.metadata.commitSymbol || `$COMMIT`}
-              </p>
-            )}
             <Card.Title>Market Price</Card.Title>
             <Card.Text>
               <span style={{ fontSize: "2rem" }}>{commitPrice || `???`}</span>
@@ -49,12 +73,15 @@ const StableReserve: React.FC = () => {
             <br />
             <br />
             <Button
+              as={`a`}
               variant={"warning"}
               children={`Go to Uniswap`}
               style={{
                 position: "absolute",
                 bottom: "1rem",
               }}
+              href={`https://app.uniswap.org/#/swap?inputCurrency=${workhardCtx?.dao.baseCurrency.address}&outputCurrency=${workhardCtx?.dao.commit.address}&use=V3`}
+              target="_blank"
             />
           </Card.Body>
         </Card>
---
+++ src/utils/uniV3.ts
@@ -0,0 +1,92 @@
+import { Contract, ethers, providers } from "ethers";
+import { Pool } from "@uniswap/v3-sdk";
+import { Token } from "@uniswap/sdk-core";
+import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+import { abi as IUniswapV3FactoryABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json";
+
+interface Immutables {
+  factory: string;
+  token0: string;
+  token1: string;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: ethers.BigNumber;
+}
+
+interface State {
+  liquidity: ethers.BigNumber;
+  sqrtPriceX96: ethers.BigNumber;
+  tick: number;
+  observationIndex: number;
+  observationCardinality: number;
+  observationCardinalityNext: number;
+  feeProtocol: number;
+  unlocked: boolean;
+}
+
+export async function getPoolAddress(
+  provider: providers.Provider,
+  token0: string,
+  token1: string,
+  fee: number
+): Promise<string> {
+  const factoryAddr = `0x1F98431c8aD98523631AE4a59f267346ea31F984`;
+  const factory = new ethers.Contract(
+    factoryAddr,
+    IUniswapV3FactoryABI,
+    provider
+  );
+  const poolAddr = await factory.getPool(token0, token1, fee);
+  return poolAddr;
+}
+
+export async function getPoolContract(
+  address: string,
+  provider: providers.Provider
+): Promise<Contract> {
+  const contract = new ethers.Contract(address, IUniswapV3PoolABI, provider);
+  return contract;
+}
+
+export async function getPoolImmutables(poolContract: Contract) {
+  const immutables: Immutables = {
+    factory: await poolContract.factory(),
+    token0: await poolContract.token0(),
+    token1: await poolContract.token1(),
+    fee: await poolContract.fee(),
+    tickSpacing: await poolContract.tickSpacing(),
+    maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
+  };
+  return immutables;
+}
+
+export async function getPoolState(poolContract: Contract) {
+  const slot = await poolContract.slot0();
+  const PoolState: State = {
+    liquidity: await poolContract.liquidity(),
+    sqrtPriceX96: slot[0],
+    tick: slot[1],
+    observationIndex: slot[2],
+    observationCardinality: slot[3],
+    observationCardinalityNext: slot[4],
+    feeProtocol: slot[5],
+    unlocked: slot[6],
+  };
+  return PoolState;
+}
+
+export async function getPool(poolContract: Contract): Promise<Pool> {
+  const immutables = await getPoolImmutables(poolContract);
+  const state = await getPoolState(poolContract);
+  const TokenA = new Token(1, immutables.token0, 18);
+  const TokenB = new Token(1, immutables.token1, 18);
+  const pool = new Pool(
+    TokenA,
+    TokenB,
+    immutables.fee,
+    state.sqrtPriceX96.toString(),
+    state.liquidity.toString(),
+    state.tick
+  );
+  return pool;
+}
